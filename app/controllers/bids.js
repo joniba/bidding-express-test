ac3ea4907dd7999dca54cae62b21b575794e4628
Ben Ami, Jonathan <Jonathan.BenAmi@ncr.com>
2015-10-12 17:47:09 +0300

    item now required and returned when creating a bid

--- app/controllers/bids.js
+++ app/controllers/bids.js
@@ -1,43 +1,56 @@
 'use strict';
 
 var Bid = require('mongoose').model('Bid');
-
+var Item = require('mongoose').model('Item');
 
 var self = {
-  getAll: function (req, res) {
+  getAll: function (req, res, next) {
     Bid.find(null, null, {limit: 30})
       .populate('item')
+      .populate('user', 'email')
       .exec(function (err, bids) {
         if (err)
-          res.send(err);
+          next(err);
 
         res.json(bids);
       });
   },
 
-  create: function (req, res) {
-    Bid.create({
-      item: req.body.itemId,
-      userId: req.user._id,
-      amount: req.body.amount
-    }, function (err, bid) {
-      if (err) {
-        res.send(err);
+  create: function (req, res, next) {
+    Item.findOne({_id: req.body.itemId}, function (err, item) {
+      if (err)
+        next(err);
+
+      if (!item) {
+        err = new Error('Associated item could not be found.');
+        err.status = 400;
+        next(err);
       }
 
-      self.getAll(req, res);
+      Bid.create({
+        item: req.body.itemId,
+        user: req.user._id,
+        amount: req.body.amount
+      }, function (err, bid) {
+        if (err) {
+          next(err);
+        }
+
+        bid.item = item;
+        res.json(bid);
+      });
     });
   },
 
-  delete: function (req, res) {
+  delete: function (req, res, next) {
     Bid.remove({
       _id: req.params.bid_id
-    }, function (err, bid) {
+    }, function (err, result) {
       if (err) {
-        res.send(err);
+        next(err);
       }
 
-      self.getAll(req, res);
+      res.json(result);
     });
   }
 };
--- app/models/bid.js
+++ app/models/bid.js
@@ -1,8 +1,20 @@
 ﻿var mongoose = require('mongoose');
 var Item = require('./item');
+var User = require('./user');
 
 module.exports = mongoose.model('Bid', {
-  item: {type: Number, ref: 'Item'},
-  userId: String,
-  amount: Number
+  item: {
+    type: Number,
+    ref: 'Item',
+    required: 'itemId is required'
+  },
+  user: {
+    type: String,
+    ref: 'User',
+    required: true
+  },
+  amount: {
+    type: Number,
+    required: true
+  }
 });
